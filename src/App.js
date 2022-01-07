0bc3aa3004aea7dbf6889e2d34170ab2b8883203
scburkha005 <scburkha005@gmail.com>
2022-01-06 21:44:21 -0800

    added useState for token and passed props

--- src/App.js
+++ src/App.js
@@ -9,20 +9,23 @@ import {
 
 function App() {
   const [posts, setPosts] = useState([]);  
+  const [token, setToken] = useState('');
+  const [message, setMessage] = useState('');
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   
   return (
    <div className="App">
-     <nav>
+     <nav className='navbar'>
+      <Link to='/'>Home</Link>
       <Link to='/login'>Login</Link>
       <Link to='/register'>Register User</Link>
      </nav>
      <Routes>
        <Route path='/' element={<Posts posts={posts} setPosts={setPosts} />}></Route>
        {/*work on login register  */}
-       <Route path='/login' element={<Login username={username} setUsername={setUsername} password={password} setPassword={setPassword}/>}></Route>
-       <Route path='/register' element={<Register username={username} setUsername={setUsername} password={password}/>}></Route>
+       <Route path='/login' element={<Login token={token} setToken={setToken} username={username} setUsername={setUsername} password={password} setPassword={setPassword}/>}></Route>
+       <Route path='/register' element={<Register token={token} setToken={setToken} username={username} setUsername={setUsername} password={password} setPassword={setPassword}/>}></Route>
      </Routes>
    </div> 
   );
