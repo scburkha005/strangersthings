89ca75cb7911c6dd7c671775aef5b3409363abc6
scburkha005 <scburkha005@gmail.com>
2022-01-06 18:57:55 -0800

    Added login message

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,22 @@
-const Login = ({password, username, setPassword, setUsername}) => {
+import { login } from '../api'
+
+const Login = ({ token, message, setToken, setMessage, password, username, setPassword, setUsername }) => {
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    const [token, message] = await login(username, password);
+    console.log('token: ', token, 'message: ', message);
+    setToken(token);
+    setMessage(message);
   }
 
   return (
     <form className='login' onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      {token && <div>{message}</div>}
       <input value={username} onChange={(event)=>{setUsername(event.target.value)}}/>
       <input value={password} onChange={(event)=>{setPassword(event.target.value)}}/>
-      <button type='submit'>Submit</button>
+      <button>Submit</button>
     </form>
   )
 }
