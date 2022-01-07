e0f6bb26de0732e116e903001c1d2f292387018b
scburkha005 <scburkha005@gmail.com>
2022-01-06 21:53:36 -0800

    conditional for message render/token

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,16 +1,23 @@
 import {useState} from 'react'
 import {register} from '../api'
 
-const Register = ({password, username, setPassword, setUsername}) => {
+const Register = ({token, password, username, setToken, setPassword, setUsername}) => {
+  const [message, setMessage] = useState('');
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    const data = await register(username, password);
-    console.log(data)
+    const [token, message] = await register(username, password);
+    setMessage(message);
+    if (token) {
+      console.log(token)
+      setToken(token);
+    }
   }
 
   return (
     <form className='register' onSubmit={handleSubmit}>
+      <h2>Register</h2>
+      {message && <div>{message}</div>}
       <input value={username} onChange={(event)=>{setUsername(event.target.value)}}/>
       <input value={password} onChange={(event)=>{setPassword(event.target.value)}}/>
       <button type='submit'>Submit</button>
