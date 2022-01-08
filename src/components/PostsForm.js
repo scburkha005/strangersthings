fd02be8514ee50cb750e1e7aec781d75cffb815b
scburkha005 <scburkha005@gmail.com>
2022-01-08 13:54:13 -0800

    Added onChange to update singlePost state

--- src/components/PostsForm.js
+++ src/components/PostsForm.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
 const PostsForm = () => {
   const blankPost = {
@@ -8,13 +8,21 @@ const PostsForm = () => {
     willDeliver: false
   }
   const [singlePost, setSinglePost] = useState(blankPost);
+  
+  useEffect(() => {
+    console.log(singlePost)
+  }, [singlePost])
+
+  const handleSubmit = async () => {
+
+  }
 
   return (
     <form className='posts-form'>
-      <input value={singlePost.title} placeholder='title'></input>
-      <input value={singlePost.description} placeholder='description'></input>
-      <input value={singlePost.price} placeholder='price'></input>
-      <input type='checkbox' value={singlePost.willDeliver} name='willDeliver'></input>
+      <input value={singlePost.title} placeholder='title' onChange={(e)=>setSinglePost({...singlePost, title: e.target.value})}></input>
+      <input value={singlePost.description} placeholder='description' onChange={(e)=>setSinglePost({...singlePost, description: e.target.value})}></input>
+      <input value={singlePost.price} placeholder='price' onChange={(e)=>setSinglePost({...singlePost, price: e.target.value})}></input>
+      <input type='checkbox' name='willDeliver' onClick={()=>setSinglePost({...singlePost, willDeliver: !singlePost.willDeliver})}></input>
       <label for='willDeliver'>Will you deliver this item?</label>
     </form>
   )
