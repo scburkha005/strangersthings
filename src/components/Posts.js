3a4b0d1b9dac4e36946b1f0edaf8d0f98febdd7f
scburkha005 <scburkha005@gmail.com>
2022-01-08 13:06:29 -0800

    added postform as child to post component

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,22 +1,26 @@
 import { useEffect } from "react";
 import { fetchPosts } from "../api";
+import PostsForm from './PostsForm';
 
-const Posts = ({posts, setPosts}) => {
+const Posts = ({posts, setPosts, token}) => {
   useEffect(() => {
     fetchPosts().then((posts) => {
       setPosts(posts)
     })
   }, [])
   return (
-    <div className = 'posts'>
-      {posts.length > 0 && posts.map(({_id, description}) => {
-        return (
-          <div key={_id}>
-            {description}
-          </div>
-        )
-      })}
-    </div>
+    <>
+      {token && <PostsForm />}
+      <div className = 'posts'>
+        {posts.length > 0 && posts.map(({_id, description}) => {
+          return (
+            <div key={_id}>
+              {description}
+            </div>
+          )
+        })}
+      </div>
+    </>
   )
 }
 export default Posts;
