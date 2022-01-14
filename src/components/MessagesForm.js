0f2f1a70d54e9f5cdefb3125db444bf2d2e7a838
scburkha005 <scburkha005@gmail.com>
2022-01-13 20:00:06 -0800

    changed to form, add preventdefault and sendmessage call

--- src/components/MessagesForm.js
+++ src/components/MessagesForm.js
@@ -1,10 +1,23 @@
-const MessagesForm = () => {
+import { useState } from 'react';
+import { sendMessage } from '../api';
+
+const MessagesForm = ({token, postid}) => {
+  const blankMessage = {
+    content: ''
+  }
+  const [message, setMessage] = useState(blankMessage);
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const data = await sendMessage(token, postid, message);
+    console.log(data);
+  }
 
   return (
-    <div className='messages-form'>
-      <input type='text' placeholder='send a message'/>
+    <form className='messages-form' onSubmit={handleSubmit}>
+      <input type='text' value={message.content} placeholder='send a message' onChange={(e) => setMessage({content: e.target.value})}/>
       <button>Send</button>
-    </div>
+    </form>
   )
 }
 
