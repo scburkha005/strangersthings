1711390dd8d012d0da12a7a96c9d3b861be11f1f
scburkha005 <scburkha005@gmail.com>
2022-01-13 19:34:45 -0800

    init messagesform

---
+++ src/components/MessagesForm.js
@@ -0,0 +1,11 @@
+const MessagesForm = () => {
+
+  return (
+    <div className='messages-form'>
+      <input type='text' placeholder='send a message'/>
+      <button>Send</button>
+    </div>
+  )
+}
+
+export default MessagesForm;
