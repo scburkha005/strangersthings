e03edebd1aeed37fca889214b2f43fb914169236
scburkha005 <scburkha005@gmail.com>
2022-01-13 19:33:23 -0800

    added load for single item view

--- src/components/PostSingle.js
+++ src/components/PostSingle.js
@@ -1,12 +1,44 @@
-const PostSingle = ({children, post: {title, description, price, willDeliver}}) => {
+import MessagesForm from "./MessagesForm";
+import { useState } from 'react';
+import { useParams, useNavigate } from "react-router";
+
+const PostSingle = ({children, setPosts, posts, post: newPost}) => {
+  const {postid} = useParams();
+  const navigate = useNavigate();
+
+  console.log(newPost)
+
+  const [post, setPost] = useState(() => {
+    if (postid) {
+      for (let i = 0; i < posts.length; i++) {
+        if (posts[i]._id === postid) {
+          return posts[i];
+        }
+      }
+    } else {
+      return newPost
+    }
+  });
+  
+  const handleChange = (e) => {
+    if (postid) {
+      navigate('/posts')
+    } else {
+      navigate(`/posts/${post._id}`)
+    }
+  }
  
   return (
     <div className='singlePost'>
-      <h4>{title}</h4>
-      <div>{description}</div>
-      <div>{price}</div>
-      {willDeliver ? <div>Delivery Available!</div> : <div>Delivery Unavailable :[</div>}
+      <h4>{post.title}</h4>
+      <div>{post.description}</div>
+      <div>{post.price}</div>
+      {post.willDeliver ? <div>Delivery Available!</div> : <div>Delivery Unavailable :[</div>}
       {children}
+      {postid && <MessagesForm />}
+      {
+        postid ? <button onClick={handleChange}>Return to Posts</button> : <button onClick={handleChange}>View Post</button>
+      }
     </div>
   )
 }
