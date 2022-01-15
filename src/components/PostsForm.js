38a87b8cd932106eebb69c473b115ac091a47409
scburkha005 <scburkha005@gmail.com>
2022-01-15 14:15:36 -0800

    added location to postform

--- src/components/PostsForm.js
+++ src/components/PostsForm.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react'
 
-const PostsForm = ({singlePost: {post: {title, description, price}}, singlePost, setSinglePost, handleSubmit}) => {
+const PostsForm = ({singlePost: {post: {title, description, price, location}}, singlePost, setSinglePost, handleSubmit}) => {
   
   useEffect(() => {
     console.log(singlePost)
@@ -8,10 +8,11 @@ const PostsForm = ({singlePost: {post: {title, description, price}}, singlePost,
 
   return (
     <form className='posts-form' onSubmit={handleSubmit}>
-      <input value={title} placeholder='title' onChange={(e)=>setSinglePost({post: {...singlePost.post, title: e.target.value}})}></input>
-      <input value={description} placeholder='description' onChange={(e)=>setSinglePost({post: {...singlePost.post, description: e.target.value}})}></input>
-      <input value={price} placeholder='price' onChange={(e)=>setSinglePost({post: {...singlePost.post, price: e.target.value}})}></input>
-      <input type='checkbox' name='willDeliver' onClick={()=>setSinglePost({post: {...singlePost.post, willDeliver: !singlePost.willDeliver}})}></input>
+      <input value={title} placeholder='title' onChange={(e)=>setSinglePost({post: {...singlePost.post, title: e.target.value}})}/>
+      <input value={description} placeholder='description' onChange={(e)=>setSinglePost({post: {...singlePost.post, description: e.target.value}})}/>
+      <input value={location} placeholder='location' onChange={(e)=>setSinglePost({post: {...singlePost, location: e.target.value}})} />
+      <input value={price} placeholder='price' onChange={(e)=>setSinglePost({post: {...singlePost.post, price: e.target.value}})}/> 
+      <input type='checkbox' name='willDeliver' onClick={()=>setSinglePost({post: {...singlePost.post, willDeliver: !singlePost.willDeliver}})}/>
       <label htmlFor='willDeliver'>Will you deliver this item?</label>
       <button>Submit</button>
     </form>
