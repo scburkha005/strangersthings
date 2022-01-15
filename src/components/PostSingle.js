12ecde53673aeb1af2df86a7d55f9ff9454c3c86
scburkha005 <scburkha005@gmail.com>
2022-01-15 13:38:00 -0800

    added messages to singlepost view

--- src/components/PostSingle.js
+++ src/components/PostSingle.js
@@ -1,14 +1,17 @@
 import MessagesForm from "./MessagesForm";
 import { useState } from 'react';
 import { useParams, useNavigate } from "react-router";
+import './PostSingle.css';
 
 const PostSingle = ({children, token, setPosts, posts, post: newPost}) => {
   const {postid} = useParams();
   const navigate = useNavigate();
+  console.log(postid)
 
   const [post, setPost] = useState(() => {
     if (postid) {
       for (let i = 0; i < posts.length; i++) {
+        console.log(posts)
         if (posts[i]._id === postid) {
           return posts[i];
         }
@@ -28,7 +31,7 @@ const PostSingle = ({children, token, setPosts, posts, post: newPost}) => {
  
   return (
     <div className='singlePost'>
-      <h4>{post.title}</h4>
+      <div className='singlePost-title'>{post.title}</div>
       <div>{post.description}</div>
       <div>{post.price}</div>
       {post.willDeliver ? <div>Delivery Available!</div> : <div>Delivery Unavailable :[</div>}
@@ -37,6 +40,14 @@ const PostSingle = ({children, token, setPosts, posts, post: newPost}) => {
       {
         postid ? <button onClick={handleChange}>Return to Posts</button> : <button onClick={handleChange}>View Post</button>
       }
+      { postid && post.messages.map((message) => {
+        return (
+          <div key={message._id} className='singlepost-singlemessage'>
+            <div>From: {message.fromUser.username}</div>
+            <div>{message.content}</div>
+          </div>
+        )
+      })}
     </div>
   )
 }
