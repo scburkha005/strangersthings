e087b0d051eb4caabfa0351335d6bd20229d6475
scburkha005 <scburkha005@gmail.com>
2022-01-16 15:40:09 -0800

    reexport

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,4 @@ export {default as AccountForm} from './AccountForm';
 export {default as PostSingle} from './PostSingle';
 export {default as Profile} from './Profile';
 export {default as Welcome} from './Welcome';
+export {default as AddPosts} from './AddPosts';
