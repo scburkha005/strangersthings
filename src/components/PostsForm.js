e375105ea94a6a4d0b2b865d2a4eb5b25a4d07e2
scburkha005 <scburkha005@gmail.com>
2022-01-16 17:01:32 -0800

    added checked property to properly control checkbox value

--- src/components/PostsForm.js
+++ src/components/PostsForm.js
@@ -3,7 +3,7 @@ import Checkbox from '@mui/material/Checkbox';
 import { FormControlLabel } from '@mui/material';
 import './PostsForm.css';
 
-const PostsForm = ({singlePost: {post: {title, description, price, location}}, singlePost, setSinglePost, handleSubmit}) => {
+const PostsForm = ({singlePost: {post: {title, description, price, location, willDeliver}}, singlePost, setSinglePost, handleSubmit}) => {
 
   return (
     <form className='posts-form' onSubmit={handleSubmit}>
@@ -11,7 +11,7 @@ const PostsForm = ({singlePost: {post: {title, description, price, location}}, s
       <TextField variant='filled' required label='description' value={description}  onChange={(e)=>setSinglePost({post: {...singlePost.post, description: e.target.value}})}/>
       <TextField variant='filled' label='location' value={location} onChange={(e)=>setSinglePost({post: {...singlePost.post, location: e.target.value}})} />
       <TextField variant='filled' required label='price' value={price} onChange={(e)=>setSinglePost({post: {...singlePost.post, price: e.target.value}})}/> 
-      <FormControlLabel label='Will you deliver this item' control={<Checkbox onChange={(e)=>setSinglePost({post: {...singlePost.post, willDeliver: !singlePost.post.willDeliver}})}/>}/>
+      <FormControlLabel label='Will you deliver this item' checked={willDeliver} control={<Checkbox onChange={(e)=>setSinglePost({post: {...singlePost.post, willDeliver: !singlePost.post.willDeliver}})}/>}/>
       <button>Submit</button>
     </form>
   )
